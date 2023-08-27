7c51b8e3843e296d8f029da7d013714f0769df0e
duytin095 <duytin095@gmail.com>
2023-08-27 23:28:29 +0700

    game over text

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,10 +10,13 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Image _liveImage;
     [SerializeField]
+    private Text _gameOverText;
+    [SerializeField]
     private Sprite[] _liveSprites;
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,8 +30,11 @@ public class UIManager : MonoBehaviour
         _scoreText.text = "Score: "+ playerScore;
     }
 
-    public void UpdateLives(int live)
+    public void UpdateLives(int currentLive)
     {
-        _liveImage.sprite = _liveSprites[live];
+        _liveImage.sprite = _liveSprites[currentLive];
+
+        if(currentLive == 0)
+            _gameOverText.gameObject.SetActive(true);
     }
 }
