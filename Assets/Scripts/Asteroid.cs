2e3f6455a1c2a217b194e75b0e4a8f5368498dea
duytin095 <duytin095@gmail.com>
2023-08-28 15:01:45 +0700

    asteroid and explosion
    asteroid explosion when collised with laser
    asteroid rotate

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,6 +6,8 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
+    [SerializeField]
+    private GameObject _explosionPrefab;
     void Start()
     {
         
@@ -15,5 +17,15 @@ public class Asteroid : MonoBehaviour
     void Update()
     {
         transform.Rotate(Vector3.forward * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Laser"))
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.25f);
+        }
     }
 }
