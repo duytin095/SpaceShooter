3b4f4a356349fac8f84dae99701485293b63b29e
duytin095 <duytin095@gmail.com>
2023-08-28 12:14:09 +0700

    main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
     {
         if(_isGameOver && Input.GetKeyUp(KeyCode.R))
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
             _isGameOver = false;
         }
     }
