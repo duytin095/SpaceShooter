94168be6cb503dad56afb5cd2129ca235adfc4f4
duytin095 <duytin095@gmail.com>
2023-08-28 14:39:33 +0700

    asteroid setup

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _speed * Time.deltaTime);
+    }
+}
